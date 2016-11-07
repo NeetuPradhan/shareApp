45750a2118873e59479a1e0e7afc73d30005da0b
neetu.pradhan <neetu@miracleinterface.com>
2016-11-07 15:40:37 +0545

    --> added div to faq,about us and tac

--- application/modules/home/views/f_a_qs.php
+++ application/modules/home/views/f_a_qs.php
@@ -6,12 +6,20 @@
 			</h4>
 		</div>
 	</div>
-	<div class="about_top">
-	    <div class="box_7">
-	   		<div class="list styled custom-list custom-list1">
-	   			<?=$cms_contents['content']?>
-	   		</div>
-	    </div>
-	    <div class="clearfix"> </div>
+	<div class="row">
+		<div class="col-lg-12">
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<div class="about_top">
+					    <div class="box_7">
+					   		<div class="list styled custom-list custom-list1">
+					   			<?=$cms_contents['content']?>
+					   		</div>
+					    </div>
+					    <div class="clearfix"> </div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
