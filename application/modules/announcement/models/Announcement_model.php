4120140c9eb49e7a9fd4a8548797aa5e19c08767
neetu.pradhan <neetu@miracleinterface.com>
2016-11-14 17:19:23 +0545

    -> announcmenet model

---
+++ application/modules/announcement/models/Announcement_model.php
@@ -0,0 +1,39 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Announcement_model extends CI_Model {
+
+	public function get_announcement($limit=false,$more_count=false) {
+		$this->db->order_by('id', 'DESC');
+		$this->db->where('status',1);
+        if($limit){
+        	$this->db->limit($limit);
+        }else if($more_count){
+        	$this->db->limit($more_count,$limit);
+        }
+        $query = $this->db->get('tbl_announcement');
+        return $query->result_array();
+	}
+
+	public function totalRecordAnnouncement() {
+		$this->db->where('status',1);
+		$query = $this->db->get('tbl_announcement');
+		$records = $query->result_array();
+		return count($records);
+	}
+
+	public function get_remaining_announcement($announcement_id) {
+		$this->db->where('id<',$announcement_id);
+		$this->db->where('status',1);
+		$this->db->limit(HOME_PAGE_LIMIT);
+        $query = $this->db->get('tbl_announcement');
+        return $query->result_array();
+	}
+
+	// public function getLimitMore($offset,$limit) {
+	// 	$sql = "select * from posts where status =  1 And del_flg=0 ORDER BY post_id DESC limit $offset,$limit";
+	// 	$cmd = Yii::app()->db->createCommand($sql);
+	// 	$cmd->execute();
+	// 	return $cmd->queryAll();
+	// }
+}
